2561eaf589eb110dbbaf4c2a336a206e77585af7
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 23:40:02 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,21 @@
-print('hello')
-print('world')
-print('hello')
-print('
+def print_table(n:int)
+    """ printthe multiplication table for numbers 1 through n inclusive.
+    >>> print_table(5)
+    
+       1  2  3   4   5 
+    1  1  2  3   4   5
+    2  2  4  6   6   10
+    3  3  6  9   12  15
+    4  4  8  12  16  20
+    5  5  10 15  20  25
+    """
+            for i in numbers:
+                 print('|t' + str(i), end = ' ')
+            print()
+            
+            for i in numbers:
+              print(i, end = ' ')
+              for j in numbers:
+                print('|t' + str(i * j), end=' ')
+                
+              print()
